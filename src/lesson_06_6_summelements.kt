86710b991022a927537b95d6bc23189a6b6f9958
seiki <seikin.alexey@gmail.com>
2021-10-21 17:19:35 +0300

    ok

--- src/lesson_06_6_summelements.kt
+++ src/lesson_06_6_summelements.kt
@@ -1,20 +1,44 @@
 import kotlin.random.Random
 
-/*считаем сумму элементов в массиве*/
-fun main(){
-
+fun main() {
+    val arrayGenerator = ArrayGenerator(10)
+    arrayGenerator.printArray()
+    println()
+    println("Сумма элементов массива: ${arrayGenerator.sum()}")
+    println("Среднее арифметическое: ${arrayGenerator.average()}")
 }
 
-class ArrayGenerator(var size: Int){
-    init {
-        val array = generateArray(size)
+class ArrayGenerator(size: Int) {
+    private val array: Array<Int>
 
+    init {
+        array = generateArray(size)
     }
 
-    private fun generateArray(s: Int): Array<Int> {
-        val array = Array(s) {0}
-        for (i in array.indices){
-            array[i] = Random.nextInt(0,100)
+    fun average(): Double {
+        return sum().toDouble() / array.size.toDouble()
+    }
+
+    fun sum(): Int {
+        var sum = 0
+        for (i in array) {
+            sum += i
+        }
+        return sum
+    }
+
+    fun printArray() {
+        print("Значения массива:")
+        for (i in array.indices) {
+            print("${array[i]}")
+            if (i < array.size - 1) print(", ")
+        }
+    }
+
+    fun generateArray(s: Int): Array<Int> {
+        val array = Array(s) { 0 }
+        for (i in array.indices) {
+            array[i] = Random.nextInt(0, 100)
         }
         return array
     }
